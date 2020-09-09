34f2dc6441b21952a54c794143a7aedae430b01d
Darya Chernova <Stomangiando@gmail.com>
2020-09-09 18:07:35 +0200

    Passworded rooms fix + guihelpers

--- BTN_Enter_PWD.cs
+++ BTN_Enter_PWD.cs
@@ -6,8 +6,17 @@ public class BTN_Enter_PWD : MonoBehaviour
     private void OnClick()
     {
         string text = BRM.CacheGameObject.Find("InputEnterPWD").GetComponent<UIInput>().label.text;
-        PhotonNetwork.JoinRoom(PanelMultiJoinPWD.roomName);
-
+        SimpleAES eaes = new SimpleAES();
+        if (text == eaes.Decrypt(PanelMultiJoinPWD.Password))
+        {
+            PhotonNetwork.JoinRoom(PanelMultiJoinPWD.roomName);
+        }
+        else
+        {
+            NGUITools.SetActive(GameObject.Find("UIRefer").GetComponent<UIMainReferences>().PanelMultiPWD, false);
+            NGUITools.SetActive(GameObject.Find("UIRefer").GetComponent<UIMainReferences>().panelMultiROOM, true);
+            GameObject.Find("PanelMultiROOM").GetComponent<PanelMultiJoin>().refresh();
+        }
     }
 }
 
